c9659fd98cb97620c05994198e5f5f8df467badf
Justin Kawaja <jkawaja909@gmail.com>
2023-04-10 20:17:07 -0230

    started defining scope of project, adding functions needed. showDealerHand to see dealer hand. readMoney and writeMoney will be used for persistent tracking of money between games.

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def main():
+def readMoney(FILENAME):
     pass
+
+def writeMoney(FILENAME):
+    pass
+
+def showDealerHand(some_array):
+    pass
+
+
+
+def main():
+    print(f"BLACKJACK!\nBlackjack payout is 3:2")
 
 
 
