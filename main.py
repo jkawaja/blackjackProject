49ba5712c1f488f5590465b0ecfe5eb1bb73b6cc
Justin Kawaja <jkawaja909@gmail.com>
2023-04-11 20:46:22 -0230

    Misread project, redoing global CARD variable.

--- main.py
+++ main.py
@@ -8,15 +8,26 @@ SUITS = ["♠", "♥", "♦", "♣"]
 # The face value of the card
 CARDS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
+CARDS = [["Heart", "2", 2], ["Heart", "3", 3], ["Heart", "4", 4], ["Heart", "5", 5], ["Heart", "6", 6],
+         ["Heart", "7", 7], ["Heart", "8", 8], ["Heart", "9", 9], ["Heart", "10", 10], ["Heart", "Jack", 10],
+         ["Heart", "Queen", 10], ["Heart", "King", 10], ["Heart", "Ace", 11], ["Diamond", "2", 2], ["Diamond", "3", 3],
+         ["Diamond", "4", 4], ["Diamond", "5", 5], ["Diamond", "6", 6], ["Diamond", "7", 7], ["Diamond", "8", 8],
+         ["Diamond", "9", 9], ["Diamond", "10", 10], ["Diamond", "Jack", 10], ["Diamond", "Queen", 10],
+         ["Diamond", "King", 10], ["Diamond", "Ace", 11], ["Spade", "2", 2], ["Spade", "3", 3], ["Spade", "4", 4],
+         ["Spade", "5", 5], ["Spade", "6", 6], ["Spade", "7", 7], ["Spade", "8", 8], ["Spade", "9", 9],
+         ["Spade", "10", 10], ["Spade", "Jack", 10], ["Spade", "Queen", 10], ["Spade", "King", 10],
+         ["Spade", "Ace", 11], ["Club", "2", 2], ["Club", "3", 3], ["Club", "4", 4], ["Club", "5", 5], ["Club", "6", 6],
+         ["Club", "7", 7], ["Club", "8", 8], ["Club", "9", 9], ["Club", "10", 10], ["Club", "Jack", 10],
+         ["Club", "Queen", 10], ["Club", "King", 10], ["Club", "Ace", 11]]
+
 dealerHand = []
 
 playerHand = []
 
-def generateDeck():
+def generateDeck(CARDS):
     deck = []
-    for suit in SUITS:
-        for card in CARDS:
-            deck.append((card, suit))
+    for card in CARDS:
+        deck.append(card)
     return deck
 
 def shuffleDeck(deck):
@@ -24,9 +35,7 @@ def shuffleDeck(deck):
     return deck
 
 def getCardFaceValue(CARDS):
-    for card in CARDS:
-        if card == "J":
-            card = 10
+    pass
 
 HAND = [('J', '♣'), ('Q', '♠')]
 
@@ -37,7 +46,12 @@ def getTotalValue(HAND):
             total += 10
         elif card[0] == "Q":
             total += 10
+        elif card[0] == "K":
+            total += 10
+        elif card[0] == "A":
+            total += 11
     return total
+
 
 
 def showDealerHand(someArray):
