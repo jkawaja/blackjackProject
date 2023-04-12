fe16700e4f8a1e03065008d79fc8fe2baa588f55
Justin Kawaja <jkawaja909@gmail.com>
2023-04-11 22:21:59 -0230

    Added functionality to game. Setting up initial dealerHand, playerHand, and if statements.

--- main.py
+++ main.py
@@ -20,10 +20,6 @@ CARDS = [["Heart", "2", 2], ["Heart", "3", 3], ["Heart", "4", 4], ["Heart", "5",
          ["Club", "7", 7], ["Club", "8", 8], ["Club", "9", 9], ["Club", "10", 10], ["Club", "Jack", 10],
          ["Club", "Queen", 10], ["Club", "King", 10], ["Club", "Ace", 11]]
 
-dealerHand = []
-
-playerHand = []
-
 def generateDeck(CARDS):
     deck = []
     for card in CARDS:
@@ -69,23 +65,52 @@ def betCalculation():
         except ValueError:
             print("Invalid input. Please try again.")
 
-def
+def dealCard(deck, hand):
+    hand.append(deck[0])
+    deck.pop(0)
+    return hand
+
 
 
 def main():
     print(f"BLACKJACK!\nBlackjack payout is 3:2")
     money = float(db.readMoney())
     keepGoing = "y"
-    while keepGoing.lower() == "y":
-        print(money)
+    # while keepGoing.lower() == "":
+    dealerHand = []
+    playerHand = []
 
-        #generate deck
-        deck = generateDeck(CARDS)
-        #shuffle deck
-        deck = shuffleDeck(deck)
-        print("shuffled deck", deck)
+    print(money)
 
-        #set up hands
+    #generate deck
+    deck = generateDeck(CARDS)
+    print("plain deck", deck)
+    #shuffle deck
+    deck = shuffleDeck(deck)
+    print("shuffled deck", deck)
+
+    #set up dealerHand
+    dealerHand = dealCard(deck, dealerHand)
+    dealerHand = dealCard(deck, dealerHand)
+    showDealerHand(dealerHand)
+    print()
+    #set up playerHand
+    playerHand = dealCard(deck, playerHand)
+    playerHand = dealCard(deck, playerHand)
+    print(f"YOUR CARDS:")
+    showHand(playerHand)
+    print()
+
+    playerChoice = input("Hit or stand? (hit/stand): ")
+    if playerChoice.lower() == 'hit':
+        playerHand = dealCard(deck, playerHand)
+        print(f"YOUR CARDS:")
+        showHand(playerHand)
+
+    #Check deck
+    print("updated deck", deck)
+
+
 
 
         #Show dealer's hand
@@ -122,11 +147,11 @@ if __name__ == '__main__':
     # deck_card_one = deck[0]
     #
     # playerHand = []
-    # playerHand.append(deck[0])
+
     # print("playerHand", playerHand)
     # handValue = getTotalValue(playerHand)
     # print("handValue", handValue)
-    # deck.pop(0)
+
     # print("deck first card popped", deck)
     #
     # testHand = [['Diamond', '5', 5], ['Club', 'Queen', 10]]
